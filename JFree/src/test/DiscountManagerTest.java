e93247fc86219e8dc3db83b11e80a2955fb1ffc2
Afnan-Sayed <afnansayed50@gmail.com>
2025-04-18 22:53:01 +0200

    Added testCalculatePriceWhenDiscountsSeasonIsFalse() function of DiscountManagerTest class using Mocking

--- JFree/src/test/DiscountManagerTest.java
+++ JFree/src/test/DiscountManagerTest.java
@@ -6,6 +6,7 @@ import org.jmock.Mockery;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
+
 public class DiscountManagerTest {
 
     @Test
@@ -15,21 +16,28 @@ public class DiscountManagerTest {
         double originalPrice = 100.0;
         double expectedPrice = 100.0;
 
+        //mocking obj
         Mockery mockingContext = new Mockery();
+
+        //mocked class
         IDiscountCalculator mockedDependency = mockingContext.mock(IDiscountCalculator.class);
         mockingContext.checking(new Expectations(){
             {
                 // make sure that none of the functions are called
+                never(mockedDependency).isTheSpecialWeek();
+                never(mockedDependency).getDiscountPercentage();
             }
         });
         DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedDependency);
         // Act
-
-
+        double actual=discountManager.calculatePriceAfterDiscount(originalPrice);
 
         // Assert
         // make sure that mocking Expectations Is Satisfied
+        mockingContext.assertIsSatisfied();
+
         // make sure that the actual value exactly equals the expected value
+        assertEquals(expectedPrice, actual);
     }
 
     @Test
