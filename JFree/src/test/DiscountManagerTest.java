59aadf9b9cf2c96543a7fca3d0e77e421a48626a
Afnan-Sayed <afnansayed50@gmail.com>
2025-04-19 00:04:45 +0200

    Added testCalculatePriceWhenDiscountsSeasonIsTrueAndSpecialWeekIsFalseAndWeekNumberIsEven () function of DiscountManagerTest class using Mocking

--- JFree/src/test/DiscountManagerTest.java
+++ JFree/src/test/DiscountManagerTest.java
@@ -1,6 +1,8 @@
 package test;
+import JFree.DiscountCalculator;
 import JFree.DiscountManager;
 import JFree.IDiscountCalculator;
+import org.jfree.data.time.Week;
 import org.jmock.Expectations;
 import org.jmock.Mockery;
 import org.junit.Test;
@@ -60,6 +62,7 @@ public class DiscountManagerTest {
                 //i used this and not 'allowing' as allowing has the probability of not calling this function,
                 //but in this test case it is a must to be called and only once
                 oneOf(mockedDependency).isTheSpecialWeek();
+
                 //and never call getDiscountPercentage
                 never(mockedDependency).getDiscountPercentage();
                 will(returnValue(expectedPrice));
@@ -79,5 +82,48 @@ public class DiscountManagerTest {
 
     }
 
-    // test missing cases
+    @Test
+    public void testCalculatePriceWhenDiscountsSeasonIsTrueAndSpecialWeekIsFalseAndWeekNumberIsEven () throws Exception
+    {
+        //Arrange
+        boolean isDiscountsSeason = true;
+
+        Week w = new Week(28, 2025);
+
+        //the concrete class to test its logic when week num id even
+        DiscountCalculator concreteCalc= new DiscountCalculator(w);
+
+        double originalPrice = 100.0;
+        double expectedPrice =concreteCalc.getDiscountPercentage(); //70.0
+
+        //mocking obj
+        Mockery mockingContext = new Mockery();
+
+        //mocked class
+        IDiscountCalculator mockedDependency = mockingContext.mock(IDiscountCalculator.class);
+
+        //expectation
+        mockingContext.checking(new Expectations(){
+            {
+                oneOf(mockedDependency).getDiscountPercentage();
+                    will(returnValue(expectedPrice));
+
+                oneOf(mockedDependency).isTheSpecialWeek();
+                    will(returnValue(false));
+            }
+        });
+
+        DiscountManager discountManager = new DiscountManager(isDiscountsSeason, mockedDependency);
+
+        // Act
+        double actual=discountManager.calculatePriceAfterDiscount(originalPrice);
+
+        // Assert
+        // make sure that mocking Expectations Is Satisfied
+        mockingContext.assertIsSatisfied();
+
+        // make sure that the actual value exactly equals the expected value
+        assertEquals(expectedPrice, actual);
+
+    }
 }
