f7bb8d688a28ad1a55028bcfaf7b1769d52e6b39
Afnan-Sayed <afnansayed50@gmail.com>
2025-04-19 18:37:28 +0200

    developed teat cases for getFirstMilliSecond(Calendar calendar)

--- JFree/src/test/test/YearTest.java
+++ JFree/src/test/test/YearTest.java
@@ -19,246 +19,19 @@ public class YearTest {
         assertEquals(2025, year.getYear());
     }
 
-//////////////////////////////// getLastMillisecond() ////////////////////////////////////
-
-    /*
-    Returns last millisecond of the year:
-
-    characteristic 1:
-        b1: relative to the time zone specified in the constructor,
-        b2: in the calendar instance passed in the most recent call to the peg(Calendar) method. (y3ne regardless tz in constr. use the one in the passed cal)
-
-    characteristic 2:
-        b1: normal year
-        b2: leap year
-    so we have 4 test cases must be covered
-
-    TODO: CHECK BOUNDARIES
-    TODO: test each characteristic alone
-     */
-
-    ////RelativeToTimeZoneInConstructor and normal year
-    @Test
-    public void testGetLastMillisecondRelativeToTimeZoneInConstructorAndIsNormalYear(){
-        //did not use .getdefault() as it produces different time zones according to which device is
-        //the code running on, and unit testing must produce the same output each time
-        TimeZone tz = TimeZone.getTimeZone("Africa/Cairo"); //verified mn java
-        Locale locale = new Locale("ar", "EG");
-
-        //badelo ay date el mohm ykon fe nafs el year ele b3mlha test '2025'
-        Date date = new GregorianCalendar(2025, Calendar.MAY, 17).getTime();
-        Year year = new Year(date, tz, locale);
-
-        long actual = year.getLastMillisecond(); //bygeb las milli second fe 2025
-
-        Calendar cal = Calendar.getInstance(tz, locale);
-        cal.set(2025, Calendar.DECEMBER, 31, 23, 59, 59);
-        cal.set(Calendar.MILLISECOND, 999);
-        long expected = cal.getTimeInMillis();
-
-        assertEquals(expected, actual);
-    }
-
-    ////RelativeToTimeZoneInConstructor and leap year
-    @Test
-    public void testGetLastMillisecondRelativeToTimeZoneInConstructorAndIsLeapYear() {
-        //did not use .getdefault() as it produces different time zones according to which device is
-        //the code running on, and unit testing must produce the same output each time
-        TimeZone tz = TimeZone.getTimeZone("Africa/Cairo"); //verified mn java
-        Locale locale = new Locale("ar", "EG");
-
-        //badelo ay date el mohm ykon fe nafs el year ele b3mlha test '2025'
-        Date date = new GregorianCalendar(2024, Calendar.MAY, 17).getTime();
-        Year year = new Year(date, tz, locale);
-
-        long actual = year.getLastMillisecond(); //bygeb las milli second fe 2025
-
-        Calendar cal = Calendar.getInstance(tz, locale);
-        cal.set(2024, Calendar.DECEMBER, 31, 23, 59, 59);
-        cal.set(Calendar.MILLISECOND, 999);
-        long expected = cal.getTimeInMillis();
-
-        assertEquals(expected, actual);
-    }
-
-    ////RelativeToTimeZoneInCalendarInPeg and normal year
-    @Test
-    public void testGetLastMillisecondRelativeToTimeZoneInCalendarInPegAndIsNormalYear() {
-        arrange();
-
-        TimeZone tz = TimeZone.getTimeZone("Africa/Cairo"); //verified mn java
-        Calendar cal = Calendar.getInstance(tz);
-        year.peg(cal);
-
-        long actual = year.getLastMillisecond();
-
-        cal.set(2025, Calendar.DECEMBER, 31, 23, 59, 59);
-        cal.set(Calendar.MILLISECOND, 999);
-        long expected = cal.getTimeInMillis();
-
-        assertEquals(expected, actual);
-    }
-
-    ////RelativeToTimeZoneInCalendarInPeg and leap year
-    @Test
-    public void testGetLastMillisecondRelativeToTimeZoneInCalendarInPegAndIsLeapYear() {
-        arrange();
-
-        TimeZone tz = TimeZone.getTimeZone("Africa/Cairo"); //verified mn java
-        Calendar cal = Calendar.getInstance(tz);
-        year.peg(cal);
-
-        long actual = year.getLastMillisecond();
-
-        cal.set(2024, Calendar.DECEMBER, 31, 23, 59, 59);
-        cal.set(Calendar.MILLISECOND, 999);
-        long expected = cal.getTimeInMillis();
-
-        assertEquals(expected, actual);
-    }
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////
-//////////////////////////////// Peg(Calendar calendar) ////////////////////////////////////
-/*
-Recalculates the start date/time and end date/time for this time period relative to the supplied calendar (which incorporates a time zone).
-Parameters: calendar - the calendar (null not permitted).
-
-Peg(Calendar calendar)
- {
-    this.firstMillisecond = this.getFirstMillisecond(calendar);
-    this.lastMillisecond = this.getLastMillisecond(calendar);
- }
-
-    characteristic 1: calendar is null
-        b1: yes -> c11
-        b2: no  -> c12
-
-    characteristic 2: normal year
-        b1: yes ->c21
-        b2: no  ->c22
-
-    characteristic 3: calendar passed with correct year
-        b1: Yes (same year as object)                    -> c31
-        b2: No (wrong year —> doesn't affect peg logic)  -> c32
-
-    neglect c11+c21+c31, c11+c21+c32, c11+c22+c31 and c11+c22+c32 as no need to test them
-    so we have 4 test cases should be covered
-
-    TODO: CHECK BOUNDARIES
-    TODO: CHECK each characteristic alone
-     */
-
-    @Test
-    public void testPegNotNullCalAndNormalYearAndCorrectYear()
-    {
-        year = new Year(2025); //year=2025
-        TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
-        Calendar calendar = Calendar.getInstance(tz);
-
-        year.peg(calendar);
-
-        //last millisecond
-        //cal is 2025 same as year -> correct year
-        calendar.set(2025, Calendar.DECEMBER, 31, 23, 59, 59);
-        calendar.set(Calendar.MILLISECOND, 999);
-        long expectedLast = calendar.getTimeInMillis();
-        assertEquals(expectedLast, year.getLastMillisecond());
-
-        //first millisecond
-        calendar.set(2025, Calendar.JANUARY, 1, 0, 0, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        long expectedFirst = calendar.getTimeInMillis();
-        assertEquals(expectedFirst, year.getFirstMillisecond());
-    }
-
-
-    @Test
-    public void testPegNotNullAndNormalYearAndWrongYear()
-    {
-        Year year = new Year(2025); //normal year
-        TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
-        Calendar calendar = Calendar.getInstance(tz);
-
-        //wrong year -> should not affect result as it will take 2025 defined in constructor
-        calendar.set(Calendar.YEAR, 2000);
-        year.peg(calendar);
-
-        //last millisecond
-        calendar.set(2025, Calendar.DECEMBER, 31, 23, 59, 59);
-        calendar.set(Calendar.MILLISECOND, 999);
-        long expectedLast = calendar.getTimeInMillis();
-        assertEquals(expectedLast, year.getLastMillisecond());
-
-
-        //first millisecond
-        calendar.set(2025, Calendar.JANUARY, 1, 0, 0, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        long expectedFirst = calendar.getTimeInMillis();
-        assertEquals(expectedFirst, year.getFirstMillisecond());
-
-    }
-
-    @Test
-    public void testPegNotNullAndLeapYearAndCorrectYear()
-    {
-        year = new Year(2024); //year=2024, leap
-        TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
-        Calendar calendar = Calendar.getInstance(tz);
-
-        year.peg(calendar);
-
-        //last millisecond
-        //cal is 2024 same as year -> correct year
-        calendar.set(2024, Calendar.DECEMBER, 31, 23, 59, 59);
-        calendar.set(Calendar.MILLISECOND, 999);
-        long expectedLast = calendar.getTimeInMillis();
-        assertEquals(expectedLast, year.getLastMillisecond());
-
-        //first millisecond
-        calendar.set(2024, Calendar.JANUARY, 1, 0, 0, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        long expectedFirst = calendar.getTimeInMillis();
-        assertEquals(expectedFirst, year.getFirstMillisecond());
-    }
-
-    @Test
-    public void testPegNotNullAndLeapYearAndWrongYear(){
-        Year year = new Year(2024); //leap year
-        TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
-        Calendar calendar = Calendar.getInstance(tz);
-
-        //wrong year -> should not affect result as it will take 2025 defined in constructor
-        calendar.set(Calendar.YEAR, 2000);
-        year.peg(calendar);
-
-        //last millisecond
-        calendar.set(2024, Calendar.DECEMBER, 31, 23, 59, 59);
-        calendar.set(Calendar.MILLISECOND, 999);
-        long expectedLast = calendar.getTimeInMillis();
-        assertEquals(expectedLast, year.getLastMillisecond());
-
-
-        //first millisecond
-        calendar.set(2024, Calendar.JANUARY, 1, 0, 0, 0);
-        calendar.set(Calendar.MILLISECOND, 0);
-        long expectedFirst = calendar.getTimeInMillis();
-        assertEquals(expectedFirst, year.getFirstMillisecond());
-    }
-
-    ////////////////////////////////////////////////////////////////////////////////////////////
-    //////////////////////////////////////// previous() ///////////////////////////////////////
+//////////////////////////////////////// previous() ///////////////////////////////////////
 /*
 @return The year preceding this one (or {@code null} if the current year is -9999).
 /*
-
     characteristic 1: year > -9999 and !=0
         b1: yes                   -> c11  //new Year(this.year - 1)
         b2: no (when year<=-9999) -> c12  //yetla3 null
 
     characteristic 2: year =0
         b1: yes                   -> c21  //new Year(this.year - 1)
-
 
     so we have 4 test cases should be covered
      */
@@ -305,7 +78,6 @@ Peg(Calendar calendar)
         assertNull(actual);
     }
 
-
 ////////////////////////////////////////////////////////////////////////////////////////////
 //////////////////////////////////////// next() ///////////////////////////////////////
 
@@ -323,9 +95,9 @@ Peg(Calendar calendar)
 
     so we have 4 test cases should be covered
      */
-@Test
-public void testNextWhenYearIsValidAndNotEqualZero()
-{
+    @Test
+    public void testNextWhenYearIsValidAndNotEqualZero()
+    {
     int y= 9998;
     Year year =new Year(y);
 
@@ -333,6 +105,7 @@ public void testNextWhenYearIsValidAndNotEqualZero()
     Year actual = (Year) year.next();
     assertEquals(9999, actual.getYear());
 }
+
     @Test
     public void testNextWhenYearIsValidAndEqualsZero()
     {
@@ -382,7 +155,6 @@ The implementation simply returns the year number (e.g. 2002).
 less than 1900
     so we have 5 test cases should be covered
 */
-
     @Test
     public void testGetSerialIndexInRange(){
         Year year = new Year(2000);
@@ -417,9 +189,214 @@ less than 1900
     }
 
 
-/////////////////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////////////////
 /////////////////////////////////////// GetFirstMillisecond() ////////////////////////////////////
     //getFirstMillisecond(Calendar calendar)
+/*
+    characteristic 1: calendar is null
+        b1: yes -> c11
+        b2: no  -> c12
+
+    characteristic 2: normal year
+        b1: yes ->c21
+        b2: no  ->c22
+
+    characteristic 3: 1900<=year<=9999
+        b1: Yes -> c31
+        b2: No  -> c32
+
+    characteristic 4: calendar passed with correct year
+        b1: Yes (same year as object)                    -> c31
+        b2: No (wrong year —> doesn't affect peg logic)  -> c32
+
+    neglect:
+     c11+c21+c31+c41
+     c11+c21+c31+c42
+     c11+c21+c32+c41
+     c11+c21+c32+c42
+     c11+c22+c31+c41
+     c11+c22+c31+c42
+     c11+c22+c32+c41
+     c11+c22+c32+c42
+
+  as no need to test them no combination when calendar is null with probability of having correct calendar
+
+     c12+c21+c31+c41
+     c12+c21+c31+c42
+     c12+c21+c32+c41
+     c12+c21+c32+c42
+     c12+c22+c31+c41
+     c12+c22+c31+c42
+     c12+c22+c32+c41
+     c12+c22+c32+c42
+
+    so we have @least 8 test cases should be covered
+
+     */
+
+
+//c12+c21+c31+c41
+@Test
+public void testGetFirstMillisecond_NotNull_NormalYear_InRange_CorrectCalendar()
+{
+    Year year = new Year(2025); // normal year
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+    Calendar calendar = Calendar.getInstance(tz);
+
+    //first millisecond of 2025
+    calendar.set(2025, Calendar.JANUARY, 1, 0, 0, 0);
+    calendar.set(Calendar.MILLISECOND, 0);
+    long expectedFirst = calendar.getTimeInMillis();
+
+    long actual = year.getFirstMillisecond(calendar);
+    assertEquals(expectedFirst, actual);
+}
+
+
+//c12+c21+c31+c42
+@Test
+public void testGetFirstMillisecond_NotNull_NormalYear_InRange_WrongCalendar()
+{
+    Year year = new Year(2025);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+
+    Calendar wrongCalendar = Calendar.getInstance(tz);
+    wrongCalendar.set(Calendar.YEAR, 2000);
+
+    Calendar expectedCalendar = Calendar.getInstance(tz);
+    expectedCalendar.set(2025, Calendar.JANUARY, 1, 0, 0, 0);
+    expectedCalendar.set(Calendar.MILLISECOND, 0);
+    long expectedFirst = expectedCalendar.getTimeInMillis();
+
+    long actual = year.getFirstMillisecond(wrongCalendar);
+    assertEquals(expectedFirst, actual);
+}
+
+//c12+c21+c32+c41
+@Test
+public void testGetFirstMillisecond_NotNull_NormalYear_LessThanMin_CorrectCalendar()
+{
+    Year year = new Year(1899);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+    Calendar calendar = Calendar.getInstance(tz);
+    assertThrows(IllegalArgumentException.class, () -> year.getFirstMillisecond(calendar));
+
+}
+
+//c12+c21+c32+c41
+@Test
+public void testGetFirstMillisecond_NotNull_NormalYear_GreaterThanMax_CorrectCalendar()
+{
+    Year year = new Year(10000);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+    Calendar calendar = Calendar.getInstance(tz);
+
+    assertThrows(IllegalArgumentException.class, () -> year.getFirstMillisecond(calendar));
+}
+
+//c12+c21+c32+c42
+@Test
+public void testGetFirstMillisecond_NotNull_NormalYear_GreaterThanMax_WrongCalendar()
+{
+    Year year = new Year(10000);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+    Calendar calendar = Calendar.getInstance(tz);
+    calendar.set(Calendar.YEAR, 2000);
+    assertThrows(IllegalArgumentException.class, () -> year.getFirstMillisecond(calendar));
+}
+
+//c12+c21+c32+c42
     @Test
-    public void testGetFirstMillisecond(){}
+public void testGetFirstMillisecond_NotNull_NormalYear_LessThanMin_WrongCalendar()
+{
+    Year year = new Year(1899);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+    Calendar calendar = Calendar.getInstance(tz);
+
+    calendar.set(Calendar.YEAR, 2000);
+    assertThrows(IllegalArgumentException.class, () -> year.getFirstMillisecond(calendar));
+
+}
+
+//c12+c22+c31+c41
+    @Test
+public void testGetFirstMillisecond_NotNull_LeapYear_InRange_CorrectCalendar()
+{
+    Year year = new Year(2024);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+    Calendar calendar = Calendar.getInstance(tz);
+
+    calendar.set(2024, Calendar.JANUARY, 1, 0, 0, 0);
+    calendar.set(Calendar.MILLISECOND, 0);
+    long expectedFirst = calendar.getTimeInMillis();
+
+    long actual = year.getFirstMillisecond(calendar);
+    assertEquals(expectedFirst, actual);
+
+}
+
+//c12+c22+c31+c42
+    @Test
+public void testGetFirstMillisecond_NotNull_LeapYear_InRange_WrongCalendar()
+{
+    Year year = new Year(2024);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+
+    Calendar wrongCalendar = Calendar.getInstance(tz);
+    wrongCalendar.set(Calendar.YEAR, 2000);
+
+    Calendar expectedCalendar = Calendar.getInstance(tz);
+    expectedCalendar.set(2024, Calendar.JANUARY, 1, 0, 0, 0);
+    expectedCalendar.set(Calendar.MILLISECOND, 0);
+    long expectedFirst = expectedCalendar.getTimeInMillis();
+
+    long actual = year.getFirstMillisecond(wrongCalendar);
+    assertEquals(expectedFirst, actual);
+
+
+}
+
+//c12+c22+c32+c41
+    @Test
+public void testGetFirstMillisecond_NotNull_LeapYear_LessThanMin_CorrectCalendar()
+{
+    Year year = new Year(1896);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+    Calendar calendar = Calendar.getInstance(tz);
+    assertThrows(IllegalArgumentException.class, () -> year.getFirstMillisecond(calendar));
+}
+
+//c12+c22+c32+c41
+    @Test
+public void testGetFirstMillisecond_NotNull_LeapYear_GreaterThanMax_CorrectCalendar()
+{
+    Year year = new Year(10000);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+    Calendar calendar = Calendar.getInstance(tz);
+    assertThrows(IllegalArgumentException.class, () -> year.getFirstMillisecond(calendar));
+}
+
+//c12+c22+c32+c42
+    @Test
+public void testGetFirstMillisecond_NotNull_LeapYear_GreaterThanMax_WrongCalendar()
+{
+    Year year = new Year(10000);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+    Calendar calendar = Calendar.getInstance(tz);
+
+    calendar.set(Calendar.YEAR, 2000);
+    assertThrows(IllegalArgumentException.class, () -> year.getFirstMillisecond(calendar));
+}
+
+//c12+c22+c32+c42
+    @Test
+public void testGetFirstMillisecond_NotNull_LeapYear_LessThanMin_WrongCalendar()
+{
+    Year year = new Year(1896);
+    TimeZone tz = TimeZone.getTimeZone("Africa/Cairo");
+    Calendar calendar = Calendar.getInstance(tz);
+
+    calendar.set(Calendar.YEAR, 2000);
+    assertThrows(IllegalArgumentException.class, () -> year.getFirstMillisecond(calendar));
+}
 }
